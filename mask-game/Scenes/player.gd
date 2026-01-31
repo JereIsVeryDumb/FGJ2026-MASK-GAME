c54da5b3588f4a065157bf9a7076bee487c4484e
JereIsVeryDumb <smrjere@gmail.com>
2026-01-31 03:47:32 +0200

    working kinda?

--- mask-game/Scenes/player.gd
+++ mask-game/Scenes/player.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
 
-# Track enemies in attack range and enemies in pickup area
-var enemies_in_range: Array = []
-var dead_enemies_in_range: Array = []  # Tracks enemies in pickup area
+# Track enemies in attack range
+var enemies_in_attack_range: Array = []
+
+# Track enemies inside pickup detector (alive or dead)
+var enemies_in_pickup_area: Array = []
 
 func _physics_process(delta: float) -> void:
 	# --- Gravity ---
@@ -37,7 +39,7 @@ func _physics_process(delta: float) -> void:
 
 	# --- Attack ---
 	if Input.is_action_just_pressed("claw"):
-		for enemy in enemies_in_range:
+		for enemy in enemies_in_attack_range:
 			if enemy and enemy.alive:
 				enemy.take_damage(1)
 				print("Attacked enemy:", enemy.name)
@@ -45,11 +47,13 @@ func _physics_process(delta: float) -> void:
 	# --- Pickup dead enemies ---
 	if Input.is_action_just_pressed("pickup"):
 		print("Pickup action pressed")
-		# Filter only dead enemies currently in range
-		var dead_in_range = dead_enemies_in_range.filter(func(e): return e.alive == false)
+		
+		# Only consider dead enemies currently inside the pickup detector
+		var dead_in_range = enemies_in_pickup_area.filter(func(e): return not e.alive)
 		print("Dead enemies in range:", dead_in_range.size())
 		for e in dead_in_range:
-			print(" - In range:", e.name, "alive?", e.alive)
+			print(" - In range:", e.name)
+
 		if dead_in_range.size() > 0:
 			pick_up_enemy(dead_in_range[0])
 
@@ -60,32 +64,31 @@ func _physics_process(delta: float) -> void:
 # --- Pickup function ---
 func pick_up_enemy(enemy):
 	print("Picked up enemy:", enemy.name)
-	dead_enemies_in_range.erase(enemy)
-	enemy.queue_free()  # Remove enemy from scene
+	enemies_in_pickup_area.erase(enemy)
+	enemy.queue_free()
 
 
 # --- Claw hurtbox signals ---
 func _on_claw_hurt_box_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemy"):
-		enemies_in_range.append(body)
+		enemies_in_attack_range.append(body)
 		print("Enemy entered attack range:", body.name)
 
 func _on_claw_hurt_box_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Enemy"):
-		enemies_in_range.erase(body)
+		enemies_in_attack_range.erase(body)
 		print("Enemy exited attack range:", body.name)
 
 
 # --- Pickup detector signals ---
 func _on_pickup_detector_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Enemy"):
-		if not dead_enemies_in_range.has(body):
-			dead_enemies_in_range.append(body)
-			print("Pickup detector entered by:", body.name, "alive?", body.alive)
+	if body.is_in_group("Enemy") and not enemies_in_pickup_area.has(body):
+		enemies_in_pickup_area.append(body)
+		print("Pickup detector entered by:", body.name, "alive?", body.alive)
 
 func _on_pickup_detector_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Enemy"):
-		# Only remove if the enemy is still alive
-		if body.alive:
-			dead_enemies_in_range.erase(body)
+		# Only remove from array if the enemy is still alive
+		if body.alive and enemies_in_pickup_area.has(body):
+			enemies_in_pickup_area.erase(body)
 			print("Pickup detector exited by:", body.name)
